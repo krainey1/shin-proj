c5898e2d04fd950a2baaf3121bab138868c728e4
krainey1 <katelynnrainey03@gmail.com>
2025-04-27 15:01:45 -0600

    condensing

--- app/create.tsx
+++ app/create.tsx
@@ -12,16 +12,7 @@ export default function createScreen() {
     <View style={{padding: 15}}>
         <Text style = {{fontSize: 20, marginTop: 10, fontWeight: "500"}}> Create Habit </Text>
         <TextInput style = {{ width: '95%', marginTop: 15, padding: 15, borderRadius: 10, borderColor: "black", borderWidth: 2, backgroundColor: "white"}} placeholder="Habit Name"/>
-        <Text style= {{fontSize: 18, fontWeight:"500"}}> Frequency </Text>
-        <View style={{flexDirection: "row", alignItems: "center", gap:10, marginVertical:10}}>
-            <Pressable style = {{backgroundColor: "#DD856F", padding: 10, borderRadius:6, flex:1}}>
-                <Text style = {{textAlign: "center", color: "white"}}> Daily </Text> 
-            </Pressable>
-            <Pressable style = {{backgroundColor: "#DD856F", padding: 10, borderRadius:6, flex:1}}>
-                <Text style = {{textAlign: "center", color: "white"}}> Weekly </Text>
-            </Pressable>
-        </View>
-        <Text style= {{fontSize: 18, fontWeight:"500"}}> On These Days </Text>
+        <Text style= {{fontSize: 18, fontWeight:"500"}}> Days To Complete </Text>
         <View style={{flexDirection: "row", alignItems:"center", gap:10, marginTop:10}}>
             {days?.map((item, index) => {
                 return (
@@ -33,7 +24,7 @@ export default function createScreen() {
         </View>
         <View style = {{marginTop: 20, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
             <Text style={{fontSize: 17, fontWeight: "500"}}> Reminder </Text>
-            <Text> Yes</Text> 
+            <Pressable style={{width: 40, height: 40, borderRadius: 5, backgroundColor: "#DD856F", justifyContent: "center", alignItems: "center"}}>  <Text style = {{color: "white"}}> Yes</Text>  </Pressable>
         </View>
         <Pressable style = {{marginRight: 25, marginLeft: 25, backgroundColor: "#DD856F", padding: 10, borderRadius: 8}}>
             <Text style = {{textAlign: "center", color: "white", fontWeight: "bold"}}> CREATE </Text>
